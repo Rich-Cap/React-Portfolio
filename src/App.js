d5065b7d33fdf799536bde4d03c57ff2b28617a3
Rich Capuy <rccapuy@gmail.com>
2020-08-09 12:55:35 -0400

    Intro template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Intro from './components/Jumbotron';
 import Project from './components/Project';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Intro />
       <Project />
       <Footer />
     </div>
