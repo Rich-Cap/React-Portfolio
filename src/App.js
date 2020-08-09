7e6fb45969360cb1740745a4f2bb479b689557f5
Rich Capuy <rccapuy@gmail.com>
2020-08-09 13:05:18 -0400

    Contact section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Intro from './components/Jumbotron';
 import Project from './components/Project';
 import About from './components/About';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,6 +14,7 @@ function App() {
       <Intro />
       <Project />
       <About />
+      <Contact />
       <Footer />
     </div>
   );
