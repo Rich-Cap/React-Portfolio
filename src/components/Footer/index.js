bd00f33820ef6452498f3fc2a3e5c2558b893689
Rich Capuy <rccapuy@gmail.com>
2020-08-09 12:49:59 -0400

    Footer template

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function Footer() {
 	return (
 		<div>
-			This is the footer section
+			<Navbar bg="dark" variant="dark" className="justify-content-center" activeKey="/home">
+				<Nav>
+					<Nav.Item>
+					<Nav.Link href="/home">Active</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+					<Nav.Link eventKey="link-1">Link</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+					<Nav.Link eventKey="link-2">Link</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+					<Nav.Link eventKey="disabled" disabled>
+						Disabled
+					</Nav.Link>
+					</Nav.Item>
+				</Nav>
+			</Navbar>
 		</div>
 	)
 }
