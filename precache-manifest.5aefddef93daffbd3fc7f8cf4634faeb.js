081586d4f63fca36d1a291ae2ecb8daccc81843a
Rich Capuy <rccapuy@gmail.com>
2020-08-13 11:23:24 -0400

    Updates

---
+++ precache-manifest.5aefddef93daffbd3fc7f8cf4634faeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9447aaac1364d551517827d2b2a2740",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "d1eb2ec0ebd541fb8cdd",
+    "url": "/React-Portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "29cd70932130d733d3e3",
+    "url": "/React-Portfolio/static/css/main.36b5738c.chunk.css"
+  },
+  {
+    "revision": "d1eb2ec0ebd541fb8cdd",
+    "url": "/React-Portfolio/static/js/2.bb1aff77.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Portfolio/static/js/2.bb1aff77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29cd70932130d733d3e3",
+    "url": "/React-Portfolio/static/js/main.93eda339.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  }
+]);
